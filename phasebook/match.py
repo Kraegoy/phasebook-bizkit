3b45f1316e869574e5646fae37d2a129ee8bf187
Kraegoy <avilakraeg@gmail.com>
2024-07-22 15:20:30 +0800

    Update match.py
    improve time complexity

--- phasebook/match.py
+++ phasebook/match.py
@@ -18,10 +18,24 @@ def match(match_id):
 
     return {"message": msg, "elapsedTime": end - start}, 200
 
+"""
+def is_match_list(fave_numbers_1, fave_numbers_2):
+    for number in fave_numbers_2:
+        if number not in fave_numbers_1:  # O(n) for each check
+            return False
+    return True
+
+# Total complexity: O(n) * O(n) = O(n^2)
+
+"""
+
 
 def is_match(fave_numbers_1, fave_numbers_2):
+    fave_numbers_set = set(fave_numbers_1)
     for number in fave_numbers_2:
-        if number not in fave_numbers_1:
+        if number not in fave_numbers_set:
             return False
-
     return True
+# Total complexity: O(n) (to create the set) + O(n) (to check membership) = O(n)
+
+
